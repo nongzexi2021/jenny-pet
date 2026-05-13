ab26945b31d4b002603897cd7f235fb3e0cf13cf
nongzexi2021 <77905663+nongzexi2021@users.noreply.github.com>
2026-05-13 08:42:13 +0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,11 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// 把 Electron 的 IPC 能力暴露给网页
 contextBridge.exposeInMainWorld('electronAPI', {
-  // 写入日历事件
   addCalendarEvent: (data) => ipcRenderer.invoke('add-calendar-event', data),
-  // 读取今天日历事件
   getCalendarEvents: () => ipcRenderer.invoke('get-calendar-events'),
-  // 写入备忘录
   addMemoNote: (data) => ipcRenderer.invoke('add-memo-note', data),
+  resizeWindow: (height) => ipcRenderer.invoke('resize-window', height),
 });
